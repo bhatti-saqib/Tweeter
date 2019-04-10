df00108585bbf5218f6ef15b54b1b1668352134c
Saqib Bhatti <saqibbhatti@saqibs-mac.local>
2019-04-10 12:26:46 -0700

    splitMessage function has been tested with UI changes.

--- Tweeter/Controllers/PostViewController.swift
+++ Tweeter/Controllers/PostViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+let numberOfCharactersPerLine = 46
 
 class PostViewController: UIViewController, UITextViewDelegate {
 
@@ -20,20 +21,108 @@ class PostViewController: UIViewController, UITextViewDelegate {
     }
     
     
-    /*@IBAction func newMessageBtnPressed(_ sender: UIButton) {
-        if let msg = tweetTextView.text {
-            print("message:\(msg)")
-        }
-        else {
-            print("Empty")
-        }
-        dismiss(animated: true, completion: nil)
-    }*/
-    
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let newMessage = tweetTextView.text {
+        if let newMessage = tweetTextView.text,
+            newMessage.count <= 50 {
             message = [newMessage]
         }
+        
+        if let newMessage = tweetTextView.text,
+            newMessage.count > 50 {
+            
+            if(!newMessage.contains(" ")) {
+                message = ["Whitespace Error"]
+            }
+            else {
+                let splitted = splitMessage(msg: newMessage)
+                message = splitted
+            }
+        }
+    }
+    
+    
+    
+    func splitMessage(msg:String) -> [String] {
+        
+        var splitMessage = [String]()
+        let arrayMessage = Array(msg)
+        var word = String()
+        var lineNumber = 1
+        var part = String()
+        
+        let lines = numberOfSplitMessages(numberOfCharacters:msg.count)
+        print(msg.count)
+        
+        part.append("\(lineNumber)/\(lines)")
+        
+        for (index, character) in arrayMessage.enumerated() {
+            
+            if((character != " ") && (index != arrayMessage.endIndex-1)) {
+                word.append(character)
+            }
+            else {
+                if(index == arrayMessage.endIndex-1) {
+                    word.append(character)
+                    part.append(" ")
+                }
+                //print("part.count:\(part.count), word.count:\(word.count)")
+                if((part.count + word.count + 1) <= 50) {
+                    if(part.last != " ") {
+                        part.append(" ")
+                    }
+                    part.append(word)
+                    word.removeAll()
+                    
+                    if(index == arrayMessage.endIndex-1)  {
+                        splitMessage.append(part)
+                    }
+                    
+                }
+                else {
+                    splitMessage.append(part)
+                    lineNumber+=1
+                    if(splitMessage.last == " ") {
+                        splitMessage.removeLast()
+                    }
+                    splitMessage.append(",")
+                    part.removeAll()
+                    part.append("\(lineNumber)/\(lines) ")
+                    part.append(word)
+                    
+                    if(index == arrayMessage.endIndex-1) {
+                        splitMessage.append(word)
+                    }
+                    word.removeAll()
+                }
+            }
+        }
+
+        print(splitMessage.count)
+        
+        for _ in splitMessage {
+            if let index = splitMessage.index(of: ",") {
+                splitMessage.remove(at: index)
+            }
+        }
+        print(splitMessage.count)
+        print(splitMessage)
+        
+        return splitMessage
+    }
+    
+    
+    func numberOfSplitMessages(numberOfCharacters:Int) -> Int {
+        let quotient = numberOfCharacters / numberOfCharactersPerLine
+        var lines = 0
+        
+        if((numberOfCharacters % numberOfCharactersPerLine) > 0) {
+            lines = quotient + 1
+        }
+        
+        if((numberOfCharacters % numberOfCharactersPerLine) == 0) {
+            lines = quotient
+        }
+        
+        return lines
     }
 }
